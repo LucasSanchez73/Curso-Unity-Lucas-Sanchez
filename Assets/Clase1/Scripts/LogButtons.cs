f7ce17d4c3d0c6c5fa4a676f663ae61d4bd47770
Lucas Sanchez <lucasjuan.sanchez@estudiantes.unahur.edu.ar>
2026-04-21 20:16:25 -0300

    Se le aplicaron mensajes a los botones de info, warning y error

--- Assets/Clase1/Scripts/LogButtons.cs
+++ Assets/Clase1/Scripts/LogButtons.cs
@@ -6,19 +6,24 @@ public class LogButtons : MonoBehaviour
     // TAREA 1: Haz que este botón muestre un mensaje normal en consola (color blanco)
     public void ShowLogInfoMessage()
     {
-        // Escribe tu código aquí:
+        Debug.Log("Mira, no rompiste nada... Por ahora");
     }
 
     // TAREA 2: Haz que este botón muestre una ADVERTENCIA (triángulo amarillo)
     public void ShowLogWarningMessage()
     {
-        // Escribe tu código aquí:
+        Debug.LogWarning("Estas a punto de hacer cagadas... Te lo adverti");
     }
 
     // TAREA 3: Haz que este botón muestre un ERROR (octágono rojo)
     // PLUS: Investiga cómo hacer que Unity se PAUSE automáticamente al pulsar este botón.
     public void ShowLogErrorMessage()
     {
-        // Escribe tu código aquí:
+        Debug.LogError("La cagaste, te lo dije... Chernobyl un poroto al lado tuyo");
+    
+        Debug.Break();
     }
+
+    
 }
+
